04087d641220720c56681364021f371feca292ac
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-06-10 15:10:30 -0300

    declaracao de funcoes basicas

---
+++ algoritmo_genetico.py
@@ -0,0 +1,13 @@
+
+def create_population(size):
+	# TO DO
+
+def create_individual(size):
+	# TO DO
+
+def convert_representation(individual):
+	# TO DO
+
+def crossover(individual1, individual2):
+	# TO DO
+
