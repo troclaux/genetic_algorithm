c53c3a96a9b0da8a4df817c91e1e0302db4dac44
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-06-17 17:23:04 -0300

    feat: termina funcao numeroAtaques()

--- hill_climbing.py
+++ hill_climbing.py
@@ -16,4 +16,29 @@ def tabuleiro(n, q):
 # def todosVizinhos(tabuleiro):
 # 	for i in range(1, len(tabuleiro)):
 
+
+def numeroAtaques(node):
+	conflict = []
+	for i in node:
+		for j in node:
+			if (i == j):
+				continue
+			if (node[i-1] == node[j-1]):
+				if ([i, j] in conflict or [j, i] in conflict):
+					continue
+				print("rainha " + str(i) + " ataca rainha " + str(j))
+				conflict.append([i, j])
+				continue
+			if (node[i-1] != node[j-1]):
+				d = abs(i-j)
+				if (node[i-1] == node[j-1] + d or node[i-1] == node[j-1] - d):
+					if ([i, j] in conflict or [j, i] in conflict):
+						continue
+					print("rainha " + str(i) + " ataca rainha " + str(j))
+					conflict.append([i, j])
+					continue
+	c = len(conflict)
+	print("numero de ataques: " + str(c))
+	return c
+
 print(tabuleiro(4,6))
